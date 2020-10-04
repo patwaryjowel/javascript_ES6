99997b7299e9c0a796d85da6fad940c032567cf5
patwaryjowel <patwaryjowel52@gmail.com>
2020-10-04 21:52:17 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,74 @@
+
+  
+console.log('Hello Jowel');
+/* =====================================
+    ES6 let & const using 
+===================================*/
+
+/* ===== const using ===== */
+
+/* 
+const arr = 10;
+console.log(arr); */
+
+/*
+arr = [];
+arr.push(10);
+arr.push(20)
+arr.push(30)
+console.log(arr)*/
+
+// ===== let using =====
+
+// let onlye work at block like { }.
+
+/* 
+let a = 10;
+
+if(true){
+    let a = 20;
+    console.log(a)
+}
+
+console.log(a) */
+
+/* 
+let x = 40; 
+function demo() {
+    let x = 10;
+    console.log(x);
+}demo(); */
+
+
+/* ===== Arrow Funtion using ===== */
+
+/* function name() {
+    console.log("Jp");
+}name()
+
+var funName = function (a, b) {
+    return a + b;
+};console.log(funName(30, 10))*/
+
+
+
+// let demoName = () => {
+//     console.log('HEllo');
+// };demoName();
+
+
+// let demoNameTwo = name => {
+//     console.log('If single params used')
+// }; demoNameTwo()
+
+/* 
+let addMe = (a,b) => {
+    return a + b;
+};console.log(addMe(10, 19)); */
+
+/* let addMeTwo = (a,b) => a+b;
+console.log(addMeTwo(10,55))
+ */
+/* let sqr = x => x*x;
+console.log(sqr(10)) */
+
